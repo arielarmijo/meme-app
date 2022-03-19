f7a894dd4d7cbe3773039e9d94b3455a7e394a87
Ariel Armijo <arielarmijo@yahoo.es>
2022-03-19 10:49:54 -0300

    change make meme layout

--- src/app/meme/pages/make-meme/make-meme.component.ts
+++ src/app/meme/pages/make-meme/make-meme.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { MemeService } from 'src/app/meme/services/meme.service';
 import { Meme } from 'src/app/meme/types/get-meme-response.interface';
@@ -8,14 +8,23 @@ import { Meme } from 'src/app/meme/types/get-meme-response.interface';
   templateUrl: './make-meme.component.html',
   styleUrls: ['./make-meme.component.css']
 })
-export class MakeMemeComponent {
+export class MakeMemeComponent implements OnInit {
 
   memes$: Observable<Meme[]>;
   currentPage = 0;
-  itemsPerPage = 8;
+  itemsPerPage = 4;
 
   constructor(private memeService: MemeService) {
     this.memes$ = this.memeService.getMemes();
+  }
+
+  ngOnInit(): void {
+    this.setItemsPerPage(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.setItemsPerPage(event.target.innerWidth);
   }
 
   get start() {
@@ -24,6 +33,22 @@ export class MakeMemeComponent {
 
   get end() {
     return this.start + (this.itemsPerPage);
+  }
+
+  private setItemsPerPage(windowWidth: number): void {
+    console.log({windowWidth});
+    if (windowWidth >= 1024) {
+      this.itemsPerPage = 16;
+      return;
+    }
+    if (windowWidth >= 768) {
+      this.itemsPerPage = 9;
+      return;
+    }
+    if (windowWidth >= 425) {
+      this.itemsPerPage = 8;
+      return;
+    }
   }
   
 }
